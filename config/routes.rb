6f86b0463adfd9ec9d3b11c2d38a7991cdf8c1b8
Ibteda Sabur <akkount35@gmail.com>
2018-11-23 02:25:06 -0500

    added some styling to home page, changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/mission'
-  get 'pages/outreach'
-  get 'pages/contact'
+  root 'pages#home'
+
+  get '/about' => 'pages#about'
+  get '/mission' => 'pages#mission'
+  get '/outreach' => 'pages#outreach'
+  get '/contact' => 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
