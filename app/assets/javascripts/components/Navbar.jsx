25a526ae29f1cc89557d75c3e54da1a42f4eff78
Ibteda Sabur <mrfruity@Mr.Fruity>
2018-12-13 22:47:37 -0500

    added basic responsiveness to navbar

--- app/assets/javascripts/components/Navbar.jsx
+++ app/assets/javascripts/components/Navbar.jsx
@@ -1,13 +1,20 @@
 class Navbar extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {isActive: "not-active"}
+    this.toggleClass=this.toggleClass.bind(this);
+
+  }
+
+  toggleClass(e){
+    this.setState({isActive: !this.state.isActive});
   }
 
   render() {
     return (
       <header id="navbar">
-        <a href="/"><img src="https://i.ibb.co/P1b6FV3/logo.png" /></a>
-        <nav>
+        <a href="/"><img src="https://i.ibb.co/P1b6FV3/logo.png" className="logo" /></a>
+        <nav className={(this.state.isActive) ? 'not-active' : 'active'}>
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
@@ -16,7 +23,7 @@ class Navbar extends React.PureComponent {
             <li><a href="/contact">Contact</a></li>
           </ul>
         </nav>
-          <div class="hamburger"><img class="hamburger" src="https://i.ibb.co/MgN6qQZ/hamburger.png"/></div>
+          <div className="hamburger" onClick={this.toggleClass}><img className="hamburger" src="https://i.ibb.co/MgN6qQZ/hamburger.png"/></div>
       </header>
     );
   }
