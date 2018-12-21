0155f29cd4ba5e71c4b74e3b3211e85eee824f9b
Ibteda Sabur <mrfruity@Mr.Fruity>
2018-12-20 22:51:38 -0500

    added dropdowns to navbar links, still working on color scheme for dropdowns and click event listeners for dropdown visibility

--- app/assets/javascripts/components/Navbar.jsx
+++ app/assets/javascripts/components/Navbar.jsx
@@ -1,13 +1,12 @@
 class Navbar extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = {isActive: "not-active"}
-    this.toggleClass=this.toggleClass.bind(this);
-
+    this.state = { isActive: "not-active" }
+    this.toggleClass = this.toggleClass.bind(this);
   }
 
-  toggleClass(e){
-    this.setState({isActive: !this.state.isActive});
+  toggleClass() {
+    this.setState({ isActive: !this.state.isActive });
   }
 
   render() {
@@ -18,12 +17,26 @@ class Navbar extends React.PureComponent {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
-            <li><a href="/mission">Projects</a></li>
-            <li><a href="/outreach">Outreach</a></li>
+            <li><a class="sub-menu" href="/mission">Projects &#11163;</a>
+              <ul>
+                <li><a href="#">Jet Kart</a></li>
+                <li><a href="#">Tesla Coil</a></li>
+                <li><a href="#">Sterling Engine</a></li>
+                <li><a href="#">More ...</a></li>
+              </ul>
+            </li>
+            <li><a class="sub-menu" href="/outreach">Outreach &#11163;</a>
+              <ul>
+                <li><a href="#">Ghost of Christmas Past</a></li>
+                <li><a href="#">Ghost of Christmas Present</a></li>
+                <li><a href="#">Ghost of Christmas Yet to Come</a></li>
+                <li><a href="#">More ...</a></li>
+              </ul>
+            </li>
             <li><a href="/contact">Contact</a></li>
           </ul>
         </nav>
-          <div className="hamburger" onClick={this.toggleClass}><img className="hamburger" src="https://i.ibb.co/MgN6qQZ/hamburger.png"/></div>
+        <div className="hamburger" onClick={this.toggleClass}><img className="hamburger" src="https://i.ibb.co/MgN6qQZ/hamburger.png" /></div>
       </header>
     );
   }
