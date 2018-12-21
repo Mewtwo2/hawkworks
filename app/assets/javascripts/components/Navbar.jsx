99e26032aa22a901a7cc0c5f046926258532d6c5
Ibteda Sabur <mrfruity@Mr.Fruity>
2018-12-20 23:42:50 -0500

    testing react state changes in conjunction with element class names

--- app/assets/javascripts/components/Navbar.jsx
+++ app/assets/javascripts/components/Navbar.jsx
@@ -1,12 +1,33 @@
 class Navbar extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.state = { isActive: "not-active" }
+    this.state = {
+      isActive: "not-active",
+      subMenuOne: "hidden",
+      subMenuTwo: "hidden"
+    }
     this.toggleClass = this.toggleClass.bind(this);
+    this.triggerMenuOne = this.triggerMenuOne.bind(this);
   }
 
   toggleClass() {
     this.setState({ isActive: !this.state.isActive });
+  }
+
+  triggerMenuOne(){
+    if(this.state.subMenuTwo == "shown"){
+      this.setState({subMenuTwo: !this.state.subMenuTwo});
+    }
+
+    this.setState({subMenuOne: !this.state.subMenuOne});
+  }
+
+  triggerMenuTwo(){
+    if(this.state.subMenuOne == "shown"){
+      this.setState({subMenuOne: !this.state.subMenuOne});
+    }
+
+    this.setState({subMenuOne: !this.state.subMenuTwo});
   }
 
   render() {
@@ -17,7 +38,7 @@ class Navbar extends React.PureComponent {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
-            <li><a class="sub-menu" href="/mission">Projects &#11163;</a>
+            <li><a onClick={this.triggerMenuOne} className={(this.state.subMenuOne) ? "hidden" : "shown"} href="/mission">Projects &#11163;</a>
               <ul>
                 <li><a href="#">Jet Kart</a></li>
                 <li><a href="#">Tesla Coil</a></li>
@@ -25,7 +46,7 @@ class Navbar extends React.PureComponent {
                 <li><a href="#">More ...</a></li>
               </ul>
             </li>
-            <li><a class="sub-menu" href="/outreach">Outreach &#11163;</a>
+            <li><a onClick={this.triggerMenuTwo} className={(this.state.subMenuTwo) ? "hidden" : "shown"} href="/outreach">Outreach &#11163;</a>
               <ul>
                 <li><a href="#">Ghost of Christmas Past</a></li>
                 <li><a href="#">Ghost of Christmas Present</a></li>
