e0e7c5a00c7468bbd2b7ca3f3bdc8afe8031fffa
Ibteda Sabur <mrfruity@Mr.Fruity>
2018-12-23 23:35:45 -0500

    fixed mobile navbar color schema change on hover, changed mobile dropdown menu to show on click instead of hover

--- app/assets/javascripts/components/Navbar.jsx
+++ app/assets/javascripts/components/Navbar.jsx
@@ -6,28 +6,39 @@ class Navbar extends React.PureComponent {
       subMenuOne: "hidden",
       subMenuTwo: "hidden"
     }
+
     this.toggleClass = this.toggleClass.bind(this);
+
     this.triggerMenuOne = this.triggerMenuOne.bind(this);
+    this.triggerMenuTwo = this.triggerMenuTwo.bind(this);
+
+    this.blockRender = this.blockRender.bind(this);
   }
 
-  toggleClass() {
+  toggleClass(e) {
     this.setState({ isActive: !this.state.isActive });
   }
 
-  triggerMenuOne(){
-    if(this.state.subMenuTwo == "shown"){
+  triggerMenuOne(e){
+    
+    if(this.state.subMenuTwo === "shown"){
       this.setState({subMenuTwo: !this.state.subMenuTwo});
     }
 
     this.setState({subMenuOne: !this.state.subMenuOne});
   }
 
-  triggerMenuTwo(){
-    if(this.state.subMenuOne == "shown"){
+  triggerMenuTwo(e){
+
+    if(this.state.subMenuOne === "shown"){
       this.setState({subMenuOne: !this.state.subMenuOne});
     }
 
-    this.setState({subMenuOne: !this.state.subMenuTwo});
+    this.setState({subMenuTwo: !this.state.subMenuTwo});
+  }
+
+  blockRender(e){
+    e.preventDefault();
   }
 
   render() {
@@ -38,20 +49,20 @@ class Navbar extends React.PureComponent {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
-            <li><a onClick={this.triggerMenuOne} className={(this.state.subMenuOne) ? "hidden" : "shown"} href="/mission">Projects &#11163;</a>
+            <li onClick={this.triggerMenuOne} className={(this.state.subMenuOne) ? "hidden" : "shown"}><a onClick={this.blockRender} href="">Projects &#11163;</a>
               <ul>
                 <li><a href="#">Jet Kart</a></li>
                 <li><a href="#">Tesla Coil</a></li>
                 <li><a href="#">Sterling Engine</a></li>
-                <li><a href="#">More ...</a></li>
+                <li><a href="/mission">More ...</a></li>
               </ul>
             </li>
-            <li><a onClick={this.triggerMenuTwo} className={(this.state.subMenuTwo) ? "hidden" : "shown"} href="/outreach">Outreach &#11163;</a>
+            <li onClick={this.triggerMenuTwo} className={(this.state.subMenuTwo) ? "hidden" : "shown"}><a onClick={this.blockRender} href="">Outreach &#11163;</a>
               <ul>
                 <li><a href="#">Ghost of Christmas Past</a></li>
                 <li><a href="#">Ghost of Christmas Present</a></li>
                 <li><a href="#">Ghost of Christmas Yet to Come</a></li>
-                <li><a href="#">More ...</a></li>
+                <li><a href="/outreach">More ...</a></li>
               </ul>
             </li>
             <li><a href="/contact">Contact</a></li>
